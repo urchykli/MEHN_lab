b1b101df38c00868399e97d6370ae97241b4110a
Sun-Mountain <npzonnenberg@gmail.com>
2019-01-18 16:19:02 -0500

    routes fin

--- routes/recipe.js
+++ routes/recipe.js
@@ -1,2 +1,10 @@
 const express = require('express')
 const router = express.Router()
+const recipeController = require('../controllers/recipe')
+
+router.post('/', recipeController.create)
+router.get('/:id', recipeController.show)
+router.patch('/:id', recipeController.update)
+router.delete('/:id', recipeController.delete)
+
+module.exports = router
--- routes/user.js
+++ routes/user.js
@@ -1,2 +1,7 @@
 const express = require('express')
 const router = express.Router()
+const userController = require('../controllers/user')
+
+router.get('/:id', userController.show)
+
+module.exports = router
