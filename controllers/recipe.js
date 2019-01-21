af25e618f08dc1e517343f91973f0ea87ca119bd
Stuart Fooks <sb.fooks@gmail.com>
2019-01-21 14:35:40 -0500

    make recipe controller

--- controllers/recipe.js
+++ controllers/recipe.js
@@ -2,9 +2,33 @@ const Recipe = require('../models/Recipe')
 
 module.exports = {
     create: (req, res) => {
-        Recipe
+        Recipe.create({
+            title: req.body.recipe.title,
+            description: req.body.recipe.description,
+            ingredients: req.body.recipe.ingredients,
+            instructions: req.body.recipe.instructions
+        })
     },
-    show:,
-    update:,
-    destroy:
+    show: (req, res) => {
+        Recipe.findOne({ _id: req.params.id })
+        .populate("user")
+        .exec(function(err, recipe) {
+            res.render("recipe/show", recipe)
+        })
+    },
+    update: (req, res) => {
+        Recipe.findOne({ _id: req.params.id})
+        .then(recipe => {
+            recipe.description = req.body.recipe.description
+        })
+        recipe.save(err => {
+            res.redirect(`recipe/${recipe._id}`)
+        })
+    },
+    destroy: (req, res) => {
+        Recipe.remove({ _id: req.params.id })
+        recipe.save(err => {
+            res.redirect(`/`)
+        })
+    }
 }
