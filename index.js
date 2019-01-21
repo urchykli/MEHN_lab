15b08f8119bf11a2a5722472a13cca84f8f64d64
Sun-Mountain <npzonnenberg@gmail.com>
2019-01-21 15:06:33 -0500

    views

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const express = require("express");
 const hbs = require("hbs");
 const bodyParser = require("body-parser");
+const app = express();
+
+hbs.registerPartials(__dirname + "/views/partials");
+
+app.set("view engine", "hbs");
+
+app.use(require("./routes/index.js"));
+
+app.listen(3000, () => console.log("server is running away... catch"));
